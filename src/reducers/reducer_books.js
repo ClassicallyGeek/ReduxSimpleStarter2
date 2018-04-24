c370a730cbde9b461bbbbac51188477703f43e25
Ashley <AshleyReneeLowry@gmail.com>
2018-04-23 19:47:31 -0500

    Create simply books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Picture of Dorian Gray' },
+    { title: 'Wuthering Heights' },
+    { title: 'Masters of Deception' },
+    { title: 'Monstress' }
+  ]
+}
